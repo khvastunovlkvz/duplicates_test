4416f6652eb444800584d5198a7baf898d4326c2
Alexey Khvastunov <khvastunovlkvz@gmail.com>
2023-07-18 23:22:35 +0300

    initCommit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] strings = {"bwe", "ewb", "ewbb", "a", "a"};
+        System.out.println(Duplicates.findDuplicates(strings));
+    }
+}
